e526a7a8a7668788cd9f8ea0d86a70a05e628466
isaacs <i@izs.me>
2011-03-14 12:06:19 -0700

    Mostly style/whitespace, a few substantive changes
    Replaced \t with "  " everywhere, and shortened lines to less than 80
    chars.
    
    Cleaned up the token.registry.js a lot, removing some unnecessary code,
    and set it up to clean out every time a token is created, if there are a
    lot of tokens already.
    
    Fixed a few actual bugs in couch.facade.js and server.js that were
    apparent once the indentation was corrected.
    
    Moved all the configuration params to environs, so there's no need to
    read through the code and replace hard-coded email info.

--- server.js
+++ server.js
@@ -1,32 +1,59 @@
 /**
  * Very lightweight server, use Express to enable a small amount of flex
  * Basic requirements for password reset via a token
- * 
+ *
  * Author: clifton.cunningham@gmail.com
- * 
- *  
  */
-var express = require('express'), 
-	tokenRegistry = require('./lib/token.registry').TokenRegistry,
-	validatorMixin = require('./lib/validator.mixin'),
-	mailer = require('./lib/emailer').Mailer,
-	check = require('validator').check,
-	sanitize = require('validator').sanitize,
-	couch = require('./lib/couch.facade');
 
 /**
  * Initial configuration of the Express server
- * 
+ *
  * Configuration from ENV
  * COUCH=localhost:5984
- * USERDB=_users 
- * ADMIN=adminuser:adminpass
+ * COUCH_USERDB=_users
+ * COUCH_ADMIN=adminuser:adminpass
+ * EMAIL_USERNAME=isaacschlueter@gmail.com
+ * EMAIL_PASSWORD=mypassword
+ * EMAIL_SMTPHOST=smtp.gmail.com
+ * EMAIL_SMTPPORT=465
+ * EMAIL_FROM=user-account-bot@npmjs.org
+ * HOSTNAME=localhost
+ * PORT=3000
  *
  **/
-var app = express.createServer();
+var envFields = [ 'COUCH',
+                  'COUCH_USERDB',
+                  'COUCH_ADMIN',
+                  'EMAIL_USERNAME',
+                  'EMAIL_PASSWORD',
+                  'EMAIL_SMTPHOST',
+                  'EMAIL_SMTPPORT',
+                  'EMAIL_FROM',
+                  'HOSTNAME',
+                  'PORT' ];
+
+var missing = envFields.filter(function (f) {
+  return undefined === process.env[f]
+})
+
+if (missing.length) {
+  throw new Error('Environment vars missing:\n'+
+                  missing.join('\n'))
+}
+
+var express = require('express')
+  , tokenRegistry = require('./lib/token.registry').TokenRegistry
+  , validatorMixin = require('./lib/validator.mixin')
+  , mailer = require('./lib/emailer').Mailer
+  , check = require('validator').check
+  , sanitize = require('validator').sanitize
+  , couch = require('./lib/couch.facade')
+  , app = express.createServer()
+
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.static(__dirname + '/public'));  // Before router to enable dynamic routing
+// Before router to enable dynamic routing
+app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 app.use(function(req, res){ res.render('404'); });
 
@@ -34,9 +61,9 @@ app.use(function(req, res){ res.render('404'); });
  * Dynamic helpers
  */
 app.dynamicHelpers({
-	request: function(req){
-	   return req;
-	}
+  request: function(req){
+     return req;
+  }
 });
 
 
@@ -48,113 +75,120 @@ app.register('.html', require('ejs'));
 app.set('view engine', 'html');
 
 /**
- * Development configuration, enables a '/list' route that shows all tokens
+ * Development configuration
+ * enables a '/list' route that shows all tokens
  */
 app.configure('development', function() {
-		
-	app.use(express.logger());
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));	
-	app.set('baseUrl','localhost:3000');
-	app.set('helpEmail','clifton.cunningham@gmail.com');
-	
-	// Enable list in dev mode
-	app.get('/list', function(req,res,next) {	
-		res.send(JSON.stringify(tokenRegistry.getCurrent()));
-	});
-	
+
+  app.use(express.logger());
+  app.use(express.errorHandler({ dumpExceptions: true,
+                                 showStack: true }));
+  var baseUrl = process.env.HOSTNAME;
+  if (process.env.PORT !== 80) baseUrl += ':' + process.env.PORT;
+  app.set('baseUrl',baseUrl);
+
+  // Enable list in dev mode
+  app.get('/list', function(req,res,next) {
+    res.send(JSON.stringify(tokenRegistry.getCurrent()));
+  });
+
 });
 
 /**
  * Production configuration, no '/list' route that shows all tokens
  */
-app.configure('production', function() {		
-	app.set('baseUrl','localhost:3000');
-	app.set('helpEmail','clifton.cunningham@gmail.com');	
-	app.use(express.errorHandler({ dumpExceptions: false, showStack: false }));
+app.configure('production', function() {
+  var baseUrl = process.env.HOSTNAME;
+  if (process.env.PORT !== 80) baseUrl += ':' + process.env.PORT;
+  app.set('baseUrl',baseUrl);
+  app.use(express.errorHandler({ dumpExceptions: false,
+                                 showStack: false }));
 });
 
 /**
  * Home page
  */
 app.get('/', function(req,res,next) {
-	res.render('home');
+  res.render('home');
 });
 
 /**
  * Post of request
  */
 app.post('/reset', function(req,res,next) {
-		
-	// Mixin params for validator
-	req.mixinParams();
 
-	var errors = "";
-	
-	req.onValidationError(function (msg) {
-    	errors += msg + "<br/>";			
+  // Mixin params for validator
+  req.mixinParams();
+
+  var errors = "";
+
+  req.onValidationError(function (msg) {
+    errors += msg + "<br/>";
+  });
+
+  //Validate user input
+  req.check('username', 'You need to enter a valid name').notEmpty();
+  req.check('email', 'You need to enter a valid email').isEmail();
+
+  req.sanitize('username').xss();
+  req.sanitize('email').xss();
+
+  var responseData;
+
+  if(errors.length == 0) {
+
+    // Ok - now lets give them a token that lasts 1 day
+    var expires = new Date((new Date()).getTime() + 1000*60*60*24);
+    var tokenId = tokenRegistry.createToken(req.body.username,
+                                            req.body.email,
+                                            'pending',
+                                            expires);
+
+    couch.validateUser(req.params.username, req.params.email,
+                       function (isValid, revision) {
+      if(isValid) {
+
+        // We need to store the revision to delete it
+        tokenRegistry.setRevision(tokenId,revision);
+
+        mailer.sendMail({ tokenId:tokenId,
+                          email:req.params.email,
+                          username:req.params.username,
+                          baseUrl:app.set('baseUrl') }, function(err,result) {
+          if(!err) {
+            responseData = {
+              message: 'Your request has been submitted, '+
+                       'if your details are valid you will '+
+                       'receive an email with further instructions.'
+            };
+            res.render("reset",{ locals: responseData });
+          } else {
+            responseData = {
+              message: 'There was a problem sending you an email:'+
+                       '<br><pre class="code">' + err + '</pre>'
+                       'This is probably because we have misconfigured '
+                        'something on the backend, please try again later.'
+            };
+            res.render("reset",{ locals: responseData });
+          }
+        });
+
+      } else {
+        responseData = {
+          message: 'That username was not found in the repository!'
+        };
+        res.render("reset",{ locals: responseData });
+      }
     });
-	
-    //Validate user input
-    req.check('username', 'You need to enter a valid name').notEmpty();
-    req.check('email', 'You need to enter a valid email').isEmail();    
-    
-    req.sanitize('username').xss();
-    req.sanitize('email').xss();    
-	
-    var responseData;
 
-    if(errors.length == 0) {
+  } else {
+    responseData = {
+      message: 'There were errors in the information you entered: '+
+               '<br><pre class="code">' + errors + '</pre>'
+    };
+    res.render("reset",{ locals: responseData });
+  }
 
-    	// Ok - now lets give them a token that lasts 1 day
-    	var expires = new Date((new Date()).getTime() + 1000*60*60*24);
-    	var tokenId = tokenRegistry.createToken(req.body.username, req.body.email, 'pending', expires);
-    	    
-    	/**
-		 * TODO: WE SHOULD MAKE SURE THE USER IS IN NPM AT THIS POINT
-		 * account details are in token.username / token.email
-		 */	
-    	couch.validateUser(req.params.username,req.params.email,function(isValid,revision) {
-    		
-    		if(isValid) {
-    			
-    			// We need to store the revision to delete it
-    			tokenRegistry.setRevision(tokenId,revision);
-    			
-	    		// SMTP settings for the emailer are in lib/emailer.js
-	        	mailer.sendMail({
-	    	    			tokenId:tokenId,
-	    	    			email:req.params.email,
-	    	    			username:req.params.username,
-	    	    			baseUrl:app.set('baseUrl'),
-	    	    			helpEmail:app.set('helpEmail')
-	    	    }, function(err,result) {	    	
-	    	    	if(!err) {
-	    	        	responseData = {message:'Your request has been submitted, if your details are valid you will receive an email with further instructions.'};
-	    	    		res.render("reset",{locals:responseData});
-	    	    	} else {
-	    	        	responseData = {message:'There was a problem sending you an email:<br/><pre class="code">' + err + '</pre>This is probably because we have misconfigured something on the backend, please try again later.'};
-	    	    		res.render("reset",{locals:responseData});		
-	    	    	}	    	
-	    	    });
-	        	
-    		} else {
-    	    	responseData = {message:'That username was not found in the repository!'};
-	    		res.render("reset",{locals:responseData});			   
-    		}
-    		
-    	});
-    	
-    	
-    	
-    	
-    	
-    } else {
-    	
-    	responseData = {message:'There were errors in the information you entered: <br><pre class="code">' + errors + '</pre>'};
-    	res.render("reset",{locals:responseData});
-    	
-    }
-       		
 });
 
 /**
@@ -162,30 +196,35 @@ app.post('/reset', function(req,res,next) {
  */
 app.get('/confirm/:tokenId', function(req,res,next) {
 
-	tokenRegistry.getToken(req.params.tokenId,function(err,token) {
-		
-		if(!err) {						
-			/**
-			 * TODO: IF YOU GET TO THIS POINT YOU CAN NOW RESET THE ACCOUNT
-			 * account details are in token.username / token.email
-			 */							
-	    	couch.deleteUser(token,function(isValid) {
-				if(isValid) {
-					tokenRegistry.removeToken(req.params.tokenId);					
-				} else {
-					err = {message:'Unable to delete the user from the repository'};					
-				}				
-	    	});			
-		};
-		
-		res.render("confirm",{locals:{err:err,token:token}});
-				
-	});	
-		
+  tokenRegistry.getToken(req.params.tokenId,function(err,token) {
+    if (err) {
+      return res.render("confirm",{ locals: { err: err, token: token } });
+    }
+
+    /**
+     * TODO: IF YOU GET TO THIS POINT YOU CAN NOW RESET THE ACCOUNT
+     * account details are in token.username / token.email
+     */
+    couch.deleteUser(token,function(isValid) {
+      if(isValid) {
+        tokenRegistry.removeToken(req.params.tokenId);
+      } else {
+        err = {
+          message: 'Unable to delete the user from the repository'
+        };
+      }
+      res.render("confirm", { locals: { err: err, token: token } })
+    });
+
+  });
+
 });
 
 
 /**
  * Launch the server
  */
-app.listen(3000);
+app.listen(process.env.PORT, function (er) {
+  if (er) throw er
+  console.log("server started on http://"+app.set('baseUrl'))
+});
